33f476ecd89d91c7915ab1499a411c6b2ccea676
bpgeck <b.geck@yahoo.com>
2015-10-26 11:49:28 -0700

    Test Networking Scene
    I have it working. Movement is slightly lagging but that's partially due
    to a shit test movement script I created in about 10 minutes

---
+++ Voxel Game/Assets/Scripts/NetworkingPlayerSetup.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.Networking; // I use the networking namespace in adition to normal engine sutff
+
+public class NetworkingPlayerSetup : NetworkBehaviour { // Notive I inherit from NetWorkBehavior rather than the usual thing
+
+    [SerializeField] // This makese componentsToDisable fillable from the Unity GUI rather than only from this script
+    Behaviour[] componentsToDisable;
+    
+	void Start () {
+        /* If we are not controlling this player (i.e. if it is beign controlled by another user),
+            then we want to disable things like movement and sound reception, that way the only thing
+            user controls do not interfere with other players */
+	    if (!isLocalPlayer)
+        {
+            for (int i = 0; i < componentsToDisable.Length; i++)
+            {
+                componentsToDisable[i].enabled = false;
+            }
+        }    
+	}
+}
