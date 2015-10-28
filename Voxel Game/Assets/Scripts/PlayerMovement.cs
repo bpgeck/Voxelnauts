de7fa1d1925e1878d881509a50baacbf7141d0dc
Marylou Robles <mrobles7@ucmerced.edu>
2015-10-27 19:09:06 -0700

    Player Movement. . . duh.
    First edition of player movement. 3D. Rare.

---
+++ Voxel Game/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour {
+
+	public float moveSpeed = 5;
+	public float roatateSpeed = 180;
+	public float jumpSpeed = 20;
+	public float gravity = 9.8f;
+
+	CharacterController controller;
+	Vector3 cuMovement;
+
+	void Start () 
+	{
+		controller = GetComponent<CharacterController>();
+	}
+
+	void Update () 
+	{
+		transform.Rotate (0, Input.GetAxis ("Horizontal") * roatateSpeed * Time.deltaTime, 0);
+		cuMovement = new Vector3(0, cuMovement.y, Input.GetAxis ("Vertical") * moveSpeed);
+		cuMovement = transform.rotation * cuMovement;
+
+		//cuMovement = new Vector3(Input.GetAxis ("Horizontal") * moveSpeed, 0, Input.GetAxis ("Vertical") * moveSpeed);
+		//controller.Move (cuMovement * Time.deltaTime);
+
+		if (!controller.isGrounded)
+			cuMovement -= new Vector3 (0, gravity * Time.deltaTime, 0);
+		else
+			cuMovement.y = 0;
+
+		if (controller.isGrounded && Input.GetButtonDown ("Jump"))
+			cuMovement.y = jumpSpeed;
+
+		controller.Move(cuMovement * Time.deltaTime);
+	}
+}
